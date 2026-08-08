7db865fcacebbdfa3af262814408a95e2c75a690
Abhijeet Gorale <abhigorale99@gmail.com>
2026-08-08 21:08:34 +0530

    Add files via upload

---
+++ CVFS/program578.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>   // only linux  System based 
+
+int main()
+{
+    int fd = 0 ;
+    int iRet = 0 ;
+
+    fd =  open("Marvellous.txt",O_RDWR | O_APPEND);    // read and write   // o_APPEND -> re write 
+
+    if(fd == -1)
+    {
+        printf("unable to open file\n");
+    }
+    else
+    {
+        printf("file get successfully opend with fd :%d\n",fd);
+        
+        iRet = write(fd,"Jay Ganesh...",13);   // location what how much(letters)
+
+        printf("%d bytes gets succesfully written",iRet);
+
+        close(fd);
+    }
+
+    return 0 ;
+}
