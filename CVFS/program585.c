291da1cb94ce782378c0d45ad3d517330535033c
Abhijeet Gorale <abhigorale99@gmail.com>
2026-08-09 21:39:10 +0530

    Add files via upload

---
+++ CVFS/program585.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<fcntl.h>   
+#include<string.h>
+
+int main()
+{
+    int fd = 0 ;
+    int iRet = 0 ;
+    char Data[100] = {'\0'} ;
+   
+    
+
+    fd =  open("Marvellous.txt",O_RDONLY);  // read only
+
+    if(fd == -1)
+    {
+        printf("unable to open file\n");
+    }
+    else
+    {
+        printf("file get successfully opend with fd :%d\n",fd);
+        
+        iRet = read(fd,Data,13);         
+
+        printf("%d bytes gets succesfully read\n",iRet);
+
+        printf("Data From file is : %s\n",Data);
+
+
+        memset(Data,'\0',sizeof(Data));      
+        
+        iRet = read(fd,Data,3);         
+
+        printf("%d bytes gets succesfully read\n",iRet);
+
+        printf("Data From file is : %s\n",Data);         
+
+
+        close(fd);
+    }
+
+    return 0 ;
+}
