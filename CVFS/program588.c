0e1ba3b1badf3f72b8b51c7c31d5339fe8d716af
Abhijeet Gorale <abhigorale99@gmail.com>
2026-08-10 13:15:46 +0530

    Add files via upload

---
+++ CVFS/program588.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<fcntl.h>   
+#include<string.h>
+
+#define BUFFER_SIZE 100    // macro
+
+
+int main()
+{
+    int fd = 0 ;
+    int iRet = 0 ;
+    char Data[BUFFER_SIZE] = {'\0'} ;
+   
+    
+
+    fd =  open("Marvellous.txt",O_RDONLY); 
+
+    if(fd == -1)
+    {
+        printf("unable to open file\n");
+    }
+    else
+    {
+        lseek(fd,5,0) ;     // fd , where , from where 
+        
+        iRet = read(fd,Data,10);  
+        
+        printf("%d bytes  gets succesfully read\n",iRet);
+
+        printf("Data from file is :%s\n",Data);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+        memset(Data,'\0',sizeof(Data));
+
+        lseek(fd,5,1) ;     // fd , where , from where 
+        
+        iRet = read(fd,Data,10);  
+        
+        printf("%d bytes  gets succesfully read\n",iRet);
+
+        printf("Data from file is :%s\n",Data);
+
+        close(fd);
+    }
+
+    return 0 ;
+}
+
+// 0 starting 
+// 1 from existance (current location)
+// 2 end 
