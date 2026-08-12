61cf0c017ac465150ad015a7361dad35beed6445
Abhijeet Gorale <abhigorale99@gmail.com>
2026-08-12 16:48:18 +0530

    Add files via upload

---
+++ CVFS/program598.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<string.h>   // memset 
+#include<unistd.h>
+
+#define BUFFER_SIZE 1024
+
+void DisplayFile(char FileName[])
+{
+    char Buffer[BUFFER_SIZE] = {'\0'};
+    int fd = 0 , iRet = 0 ;
+
+    fd = open(FileName,O_RDONLY);
+
+    if(fd == -1)
+    {
+        printf("Unable to open file\n");
+        return  ;
+    }
+
+    while((iRet = read(fd,Buffer,sizeof(Buffer))) !=0 )
+    {
+        printf("%s",Buffer);
+
+        memset(Buffer,'\0',sizeof(Buffer));
+    }
+
+    close(fd);
+}
+
+int main()
+{
+    
+    char fName[30] = {'\0'};
+
+    printf("Enter the file name :\n");
+    scanf("%[^'\n]s",fName);
+
+    DisplayFile(fName);
+    
+    return 0 ;
+}
