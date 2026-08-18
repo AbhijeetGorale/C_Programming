39e8389e6aa50090d53c166d75e343ed9d9d2d21
Abhijeet Gorale <abhigorale99@gmail.com>
2026-08-18 16:19:24 +0530

    Add files via upload

---
+++ CVFS/program628.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int iRet = 0 ;
+    int i = 0 , j = 0 , k = 0 ;
+
+    printf("Enter three numbers:\n");
+    iRet = scanf("%d %d %d",&i,&j,&k);
+
+    printf("value of iRet is :%d\n",iRet);
+
+
+
+    return 0 ;
+}
