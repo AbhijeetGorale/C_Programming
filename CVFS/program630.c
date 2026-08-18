3399e57f75937ad99371ac7ddc7f617ce386c0b7
Abhijeet Gorale <abhigorale99@gmail.com>
2026-08-18 16:20:30 +0530

    Add files via upload

---
+++ CVFS/program630.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+{
+    char str[80] = {'\0'};
+
+    // char Command1[20];
+    // char Command2[20];
+    // char Command3[20];
+
+    char Command[4][20] = {{'\0'}};
+
+
+    printf("Enter Command :\n");
+    fgets(str,sizeof(str),stdin);  // scanf
+
+    printf("Entered command is :%s\n",str);
+
+    sscanf(str,"%s %s %s",Command[0],Command[1],Command[2]);  
+    
+    printf("First token %s\n",Command[0]);
+    printf("second token %s\n",Command[1]);
+    printf("third token %s\n",Command[2]);
+
+
+
+
+
+    return 0 ;
+}
