0da9dcf84224784ab24082a1dd28dc5b321d10b6
Abhijeet Gorale <abhigorale99@gmail.com>
2026-08-20 20:09:37 +0530

    Add files via upload

---
+++ CVFS/program640.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<string.h>
+#include<stdbool.h>
+    
+
+
+int main()
+{
+    system("dir");         // use to pass the command to cmd from prog
+
+
+    return  0 ; 
+}
